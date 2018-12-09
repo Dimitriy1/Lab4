37d46d197e8739d9c53b243f3cdff135922237c2
Dimitriy1 <dimalitvin192.0@gmail.com>
2018-12-09 19:42:46 +0200

    Delete Main.java

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Thread thread1 = new TmpThread(5000, "Thread_1");
-        Thread thread2 = new TmpThread(3000, "Thread_2");
-        thread1.start();
-        thread2.start();
-        System.out.println("main method finished");
-    }
-}
