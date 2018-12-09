e59f981660d0bac37c75cddd9fee9128e8bcce90
Dimitriy1 <dimalitvin192.0@gmail.com>
2018-12-09 19:56:15 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        
         Thread thread1 = new TmpThread(5000, "Thread_1");
         Thread thread2 = new TmpThread(3000, "Thread_2");
         thread1.start();
